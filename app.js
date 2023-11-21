29734f5b080a7e5525549487dfe91be8607a2219
SkSelim-Git <skselim0777@gmail.com>
2023-11-21 23:49:51 +0530

    add new code of todo app

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
-let todo = [];
+let btn = document.querySelector('button');
+let ul = document.querySelector('ul');
+let inp = document.querySelector('input');
 
-let req = prompt("please  enter your request");
+btn.addEventListener("click", function(){
+    let item = document.createElement('li');
+    item.innerText = inp.value;
+    ul.appendChild(item);
 
-while(true){
-    if(req == "quit"){
-        console.log("quitting app");
-        break;
+    let delbtn = document.createElement('button');
+    delbtn.innerText = "delete";
+    delbtn.classList.add("delete");
+    item.appendChild(delbtn);
+    
+    inp.value = "";
+})
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName){
+       let listItem = event.target.parentElement;
+       listItem.remove();
     }
-    if(req == "list"){
-        console.log("------------------");
-        for(let i=0; i<=req.length; i++){
-            console.log(i, todo[i]);
-        }
-        console.log("------------------");
-    } else if(req == "add"){
-        let task = prompt("please enter your task that you want to add");
-        todo.push(task);
-        console.log("task added");
-    } else if(req == "delete"){
-        let idx = prompt("please eneter the task index")
-        todo.splice(idx, 1);
-        console.log("tesk delete");
-    }else{
-        console.log("wrong request");
-    }
-    req = prompt("please  enter your request"); 
-}
+})
